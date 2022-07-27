dc183504af2ca87ccc045ff3a13e1432afc757c4
ed_bolma <ed.bolma21@gmail.com>
2022-07-27 09:08:39 +0200

    Sectoin is not working has to be fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,15 @@ import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
 export const App = () => {
-  const options = ['good', 'neutral', 'bad'];
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const options = {
+    good,
+    neutral,
+    bad,
+  };
 
   const handleClick = option => {
     switch (option) {
@@ -39,76 +43,26 @@ export const App = () => {
 
   return (
     <div className={css.App}>
-      <Section title={'Please leave feadback'} titleType={'h1'}>
-        <FeedbackOptions options={options} onLeaveFeedback={handleClick} />
+      <Section title="Please leave feadback" titleType="h1">
+        <FeedbackOptions
+          options={Object.keys(options)}
+          onLeaveFeedback={handleClick}
+        />
       </Section>
 
       {Boolean(countTotalFeedback()) ? (
-        <Section title={'Statistics'} titleType={'h2'}>
+        <Section title="Statistics" titleType="h2">
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={options.good}
+            neutral={options.neutral}
+            bad={options.bad}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage().toFixed()}
           />
         </Section>
       ) : (
-        <Notification message={'There is no feedback yet'} />
+        <Notification message="There is no feedback yet" />
       )}
     </div>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleClick = option => {
-//     this.setState(prevState => ({
-//       [option]: prevState[option] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = () =>
-//     this.state.good + this.state.neutral + this.state.bad;
-
-//   countPositiveFeedbackPercentage = () => {
-//     const total = this.countTotalFeedback();
-//     const positiveFeedback = this.state.good;
-
-//     return (positiveFeedback / total) * 100 || 0;
-//   };
-
-//   render() {
-//     const options = Object.keys(this.state);
-
-//     return (
-//       <div className={css.App}>
-//         <Section title={'Please leave feadback'} titleType={'h1'}>
-//           <FeedbackOptions
-//             options={options}
-//             onLeaveFeedback={this.handleClick}
-//           />
-//         </Section>
-
-//         {Boolean(this.countTotalFeedback()) ? (
-//           <Section title={'Statistics'} titleType={'h2'}>
-//             <Statistics
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage().toFixed()}
-//             />
-//           </Section>
-//         ) : (
-//           <Notification message={'There is no messages yet'} />
-//         )}
-//       </div>
-//     );
-//   }
-// }
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,28 +1,23 @@
-import { Component } from 'react/cjs/react.production.min';
+// import { Component } from 'react/cjs/react.production.min';
+import { useState } from 'react';
 import css from './Section.module.css';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-class Section extends Component {
-  state = {
-    title: this.props.title,
-    hType: this.props.titleType,
-  };
+const Section = ({ title, titleType, children }) => {
+  const [titleSection, setTitleSection] = useState();
+  const [hType, setHType] = useState();
 
-  render() {
-    Section.propTypes = {
-      title: PropTypes.string.isRequired,
-      titleType: PropTypes.string.isRequired,
-      children: PropTypes.any,
-    };
-    return (
-      <div>
-        <this.state.hType className={css.test}>
-          {this.props.title}
-        </this.state.hType>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+  setTitleSection(title);
+  setHType(titleType);
+
+  const TitleTag = `h${hType}`;
+
+  return (
+    <div>
+      <TitleTag className={css.test}>{titleSection}</TitleTag>
+      {children}
+    </div>
+  );
+};
 
 export default Section;
